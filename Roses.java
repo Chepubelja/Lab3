d687b7c1978e13c8d4dc22c3ae24f2acd125e09e
Chepubelja <rehusevych@ucu.edu.ua>
2016-10-10 15:11:19 +0300

    Delete Roses.java

--- Roses.java
+++
@@ -1,9 +0,0 @@
-public class Roses extends Flowers {
-    boolean thornity;
-    Roses(int price, int freshness, double stemSize, boolean thornity, String name) {
-        super(price, freshness, stemSize, name);
-    }
-    public void setThornity(boolean thorn){
-        thornity = thorn;
-    }
-}
